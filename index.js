4efeecf5aaa7a95adbc78fa91787069901ffd5c8
bayot01 <bayotzaripov@gmail.com>
2021-07-15 09:10:23 +0500

    raund cheksiz qilindi

--- index.js
+++ index.js
@@ -1,84 +1,69 @@
-let playerOchko=0
-let botOchko=0
-for(let i=0; i<6; i++){
-    let player=prompt("tosh,qaychi,qog'oz,tanlang").toLowerCase()
-    let bot = ['tosh',"qog'oz",'qaychi']
-    botTanlovi=bot[Math.round(Math.random()*2)]
-    
-    if (player==botTanlovi){
-        console.log(player)
-        console.log(botTanlovi)
-        alert('durrang')
-    } else if(player=='tosh'&&botTanlovi=="qaychi"){
-        console.log(player)
-        console.log(botTanlovi)
-        alert("player yutti")
-        playerOchko++
-        console.log(playerOchko)
-    } else if(player=='tosh'&&botTanlovi=="qog'oz"){
-        console.log(player)
-        console.log(botTanlovi)
-        alert("bot yutti")
-        botOchko++
-        console.log(botOchko)
-    } else if(player=="qog'oz"&&botTanlovi=="tosh"){
-        console.log(player)
-        console.log(botTanlovi)
-        alert("player yutti")
-        playerOchko++
-        console.log(playerOchko)
-    } else if(player=="qog'oz"&&botTanlovi=="qaychi"){
-        console.log(player)
-        console.log(botTanlovi)
-        alert("bot yutti")
-        botOchko++
-        console.log(botOchko)
-    } else if(player=='qaychi'&&botTanlovi=='tosh'){
-        console.log(player)
-        console.log(botTanlovi)
-        alert("bot yutti")
-        botOchko++
-        console.log(botOchko)
-    } else if(player=='qaychi'&&botTanlovi=="qog'oz"){
-        console.log(player)
-        console.log(botTanlovi)
-        alert('player yutti')
-        playerOchko++
-        console.log(playerOchko)
+let playerOchko = 0
+let botOchko = 0
+let cheksiz = true
+function game() {
+  for (let i = 0; i < 3; i++) {
+    let player = prompt("tosh,qaychi,qog'oz,tanlang").toLowerCase()
+    let bot = ['tosh', "qog'oz", 'qaychi']
+    botTanlovi = bot[Math.round(Math.random() * 2)]
+    if (player == botTanlovi) {
+      console.log(player)
+      console.log(botTanlovi)
+      alert('durrang')
+    } else if (player == 'tosh' && botTanlovi == "qaychi") {
+      console.log(player)
+      console.log(botTanlovi)
+      alert("player yutti")
+      playerOchko++
+      console.log(playerOchko)
+    } else if (player == 'tosh' && botTanlovi == "qog'oz") {
+      console.log(player)
+      console.log(botTanlovi)
+      alert("bot yutti")
+      botOchko++
+      console.log(botOchko)
+    } else if (player == "qog'oz" && botTanlovi == "tosh") {
+      console.log(player)
+      console.log(botTanlovi)
+      alert("player yutti")
+      playerOchko++
+      console.log(playerOchko)
+    } else if (player == "qog'oz" && botTanlovi == "qaychi") {
+      console.log(player)
+      console.log(botTanlovi)
+      alert("bot yutti")
+      botOchko++
+      console.log(botOchko)
+    } else if (player == 'qaychi' && botTanlovi == 'tosh') {
+      console.log(player)
+      console.log(botTanlovi)
+      alert("bot yutti")
+      botOchko++
+      console.log(botOchko)
+    } else if (player == 'qaychi' && botTanlovi == "qog'oz") {
+      console.log(player)
+      console.log(botTanlovi)
+      alert('player yutti')
+      playerOchko++
+      console.log(playerOchko)
     } else {
-        alert("Faqat tosh qaychi qog'oz so'zlarini kiriting")
+      alert("Faqat tosh qaychi qog'oz so'zlarini kiriting")
     }
+  }
 }
-// if (player==botTanlovi){
-//     console.log(player)
-//     console.log(botTanlovi)
-//     alert('durrang')
-// } else if(player=='tosh'&&botTanlovi=="qaychi"){
-//     console.log(player)
-//     console.log(botTanlovi)
-//     alert("player yutti")
-// } else if(player=='tosh'&&botTanlovi=="qog'oz"){
-//     console.log(player)
-//     console.log(botTanlovi)
-//     alert("bot yutti")
-// } else if(player=="qog'oz"&&botTanlovi=="tosh"){
-//     console.log(player)
-//     console.log(botTanlovi)
-//     alert("player yutti")
-// } else if(player=="qog'oz"&&botTanlovi=="qaychi"){
-//     console.log(player)
-//     console.log(botTanlovi)
-//     alert("bot yutti")
-// } else if(player=='qaychi'&&botTanlovi=='tosh'){
-//     console.log(player)
-//     console.log(botTanlovi)
-//     alert("bot yutti")
-// } else if(player=='qaychi'&&botTanlovi=="qog'oz"){
-//     console.log(player)
-//     console.log(botTanlovi)
-//     alert('player yutti')
-// } else {
-//     alert("Faqat tosh qaychi qog'oz so'zlarini kiriting")
-// }
+function restart() {
+  let savol = confirm("yana o'ynashni hohlaysizmi")
+  if (savol == true) {
+    game()
+    cheksiz=true
+  } else if (savol == false) {
+    alert("o'yin tugadi")
+    cheksiz=false
+  }
+}
+while(cheksiz==true){
+game()
+restart()
+}
 
 
